6edfe221e7a5a7a2fca1e4fab523013ee5aa5db1
chenchenqin <602523219@qq.com>
2016-10-31 22:15:38 +0800

    midterm

--- js/ex_js001.js
+++
@@ -1,4 +0,0 @@
-function sayHello(){
-    console.log("Hello");
-    $("div#output").text("Hello");
-} 
--- js/ex_js002.js
+++
@@ -1,5 +0,0 @@
-function sayHello(){
-    var str=$("#input").val();
-    $("#output").text(str);
-}
-$("#test").bind('click',sayHello);
