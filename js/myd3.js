bfb4e7ff621b6fb4a795655c5c3f4105dfcb6992
chenchenqin <602523219@qq.com>
2016-11-07 15:44:19 +0800

    d3

---
+++ js/myd3.js
@@ -0,0 +1,28 @@
+var svgctrl=d3.select('svg');
+
+var cx
+
+//<polyline points="380,150 390,140 400,150" style="fill:none;stroke:#F5D0A9;stroke-width:3" />
+//<polyline points="130,120 120,100 100,100" style="fill:none;stroke:#F5D0A9;stroke-width:3" />
+//<polyline points="170,120 160,100 140,100" style="fill:none;stroke:#F5D0A9;stroke-width:3" />
+        
+//<circle cx="135" cy="145" r="4" stroke="#848484" stroke-width="3" fill="#848484" />
+//<circle cx="165" cy="145" r="4" stroke="#848484" stroke-width="3" fill="#848484" />
+//<polyline points="145,155 150,165 155,155" style="fill:none;stroke:#0080FF;stroke-width:3" />
+        
+//<circle cx="240" cy="200" r="10" stroke="pink" stroke-width="3" fill="pink" />
+//<circle cx="320" cy="200" r="10" stroke="pink" stroke-width="3" fill="pink" />
+        
+//<rect x="50" y="220" width="350" height="10"style="fill:#cccfff;stroke:#cccfff;stroke-width:5;"/>
+
+var c1=svgctrl.append('circle');
+c1.attr('cx',150).attr('cy',150).attr('r',40).attr('fill','pink');
+
+var c2=svgctrl.append('ellipse');
+c2.attr('cx',280).attr('cy',150).attr('rx',100).attr('ry',50).attr('fill','pink').attr('stroke-width',2);
+
+var c3=svgctrl.append('polyline');
+c3.attr('points',);
+
+var c4=svgctrl.append();
+c4.attr();
