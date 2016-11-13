c895e136deafd05d216b460445ba8214ec59ffeb
chenchenqin <602523219@qq.com>
2016-11-13 21:37:37 +0800

    2016/11/07

--- js/myd3.js
+++ js/myd3.js
@@ -1,28 +1,45 @@
 var svgctrl=d3.select('svg');
-
-var cx
-
-//<polyline points="380,150 390,140 400,150" style="fill:none;stroke:#F5D0A9;stroke-width:3" />
-//<polyline points="130,120 120,100 100,100" style="fill:none;stroke:#F5D0A9;stroke-width:3" />
-//<polyline points="170,120 160,100 140,100" style="fill:none;stroke:#F5D0A9;stroke-width:3" />
-        
-//<circle cx="135" cy="145" r="4" stroke="#848484" stroke-width="3" fill="#848484" />
-//<circle cx="165" cy="145" r="4" stroke="#848484" stroke-width="3" fill="#848484" />
-//<polyline points="145,155 150,165 155,155" style="fill:none;stroke:#0080FF;stroke-width:3" />
-        
-//<circle cx="240" cy="200" r="10" stroke="pink" stroke-width="3" fill="pink" />
-//<circle cx="320" cy="200" r="10" stroke="pink" stroke-width="3" fill="pink" />
-        
-//<rect x="50" y="220" width="350" height="10"style="fill:#cccfff;stroke:#cccfff;stroke-width:5;"/>
-
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("svg").hide();
+    });
+    $("#show").click(function(){
+        $("svg").show();
+    });
+});
+var cx=150;
+var cy=150; 
+var r=40;      
 var c1=svgctrl.append('circle');
-c1.attr('cx',150).attr('cy',150).attr('r',40).attr('fill','pink');
+c1.attr('cx',cx).attr('cy',cy).attr('r',r).attr('fill','pink');
 
 var c2=svgctrl.append('ellipse');
-c2.attr('cx',280).attr('cy',150).attr('rx',100).attr('ry',50).attr('fill','pink').attr('stroke-width',2);
+c2.attr('cx',cx+130).attr('cy',cy).attr('rx',r*2.5).attr('ry',r*1.25).attr('fill','pink').attr('stroke-width',2);
 
-var c3=svgctrl.append('polyline');
-c3.attr('points',);
+//var c3=svgctrl.append('polyline');
+//c3.attr('points',"380,150,390,140,400,150").attr('fill','none').attr('stroke','#F5D0A9').attr('stroke-width',3);
 
-var c4=svgctrl.append();
-c4.attr();
+//var c4=svgctrl.append('polyline');
+//c4.attr('points',"130,120 120,100 100,100").attr('fill','none').attr('stroke','#F5D0A9').attr('stroke-width',3);
+
+//var c5=svgctrl.append('polyline');
+//c5.attr('points',"170,120 160,100 140,100").attr('fill','none').attr('stroke','#F5D0A9').attr('stroke-width',3);
+
+var c6=svgctrl.append('circle');
+c6.attr('cx',cx-15).attr('cy',cy-5).attr('r',r*0.1).attr('stroke','#848484').attr('stroke-width',3).attr('fill','#848484');
+
+var c7=svgctrl.append('circle');
+c7.attr('cx',cx+15).attr('cy',cy-5).attr('r',r*0.1).attr('stroke','#848484').attr('stroke-width',3).attr('fill','#848484');
+
+//var c8=svgctrl.append('polyline');
+//c8.attr('points',"145,155 150,165 155,155").attr('fill','none').attr('stroke','#0080FF').attr('stroke-width',3);
+
+var c9=svgctrl.append('circle');
+c9.attr('cx',cx+90).attr('cy',cy+50).attr('r',r*0.25).attr('stroke','pink').attr('stroke-width',3).attr('fill','pink');
+
+var c10=svgctrl.append('circle');
+c10.attr('cx',cx+170).attr('cy',cy+50).attr('r',r*0.25).attr('stroke','pink').attr('stroke-width',3).attr('fill','pink');
+
+var c11=svgctrl.append('rect');
+c11.attr('x',cx-100).attr('y',cy+70).attr('width',350).attr('height',10).attr('fill','#cccfff').attr('stroke-width',5).attr('stroke','#cccfff');
+
